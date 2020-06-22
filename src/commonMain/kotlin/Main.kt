948b5d78686b9d9c2761d67785a663726b4368ee
Chiara Lang <chiara.l.lang@icloud.com>
2020-06-22 20:20:59 +0200

    Penguin jumps on command

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import com.soywiz.korev.Key
 import com.soywiz.korge.view.addUpdater
 import com.soywiz.korim.color.Colors
 import me.emig.engineEmi.*
@@ -8,24 +9,27 @@ suspend fun main() = Engine(engineConfig) {
 	//val hut = Bild(150,20,"hut.png")
 	//addChild(hut)
 
-    val spaceArray : DoubleArray =  doubleArrayOf(10.0, 50.0, 200.0)
-
-
-	val iceFloe1 = IceFloe(100, 600, 1000, 50, 3.0)
-	addChild(iceFloe1)
-	val iceFloe2 = IceFloe(iceFloe1.width + 10, 0, 1000, 50, 3.0, Colors.PURPLE)
-	addChild(iceFloe2)
-	//val iceFloe3 = IceFloe(1100, 0, 1000, 50, 3.0, Colors.PURPLE)
-	//iceFloe1.addChild(iceFloe3)
+	val input = views.input
 
 	val penguin = Player (100, 100, 20, 50, 0.0, Colors.RED)
 	addChild(penguin)
 
-	val a = 100.0 //Beschleunigung in y-Richtung -> ≙ Erdanziehung g
+    val spaceArray : DoubleArray =  doubleArrayOf(10.0, 50.0, 200.0)
+
+	val iceFloe1 = IceFloe(100, 600, 1000, 50, 3.0)
+	addChild(iceFloe1)
+	//val iceFloe2 = IceFloe(iceFloe1.width + 10, 0, 1000, 50, 3.0, Colors.PURPLE)
+	//addChild(iceFloe2)
+	//val iceFloe3 = IceFloe(1100, 0, 1000, 50, 3.0, Colors.PURPLE)
+	//iceFloe1.addChild(iceFloe3)
+
+
+	val ay = 100.0 //Beschleunigung in y-Richtung -> ≙ Erdanziehung g
 	val ax = 10.0 //Beschleunigung in x-Richtung -> Schwierigkeitsgrad erhöhen
 
 	//vy -> Geschwindigkeit in y-Richtung (Pinguin)
 	//vx -> Geschwindigkeit in x-Richtung (Eisscholle)
+
 
 	addUpdater { dt ->
 		//iceFloe wird in x Richtung mit ax beschleunigt
@@ -36,18 +40,22 @@ suspend fun main() = Engine(engineConfig) {
 
 		iceFloe1.vx = ifvxNeu
 
-		//penguin wird in y Richtung mit a beschleunigt (Fall)
+		//penguin wird in y Richtung mit ay beschleunigt
 		val pvyAlt = penguin.vy
-		val pvyNeu = penguin.vy + a*dt.seconds
+		val pvyNeu = penguin.vy + ay*dt.seconds
 
 		penguin.y += 0.5*(pvyAlt + pvyNeu) * dt.seconds
 
 		penguin.vy = pvyNeu
 
-		//penguin stößt sich mit vy ab, wenn er die Scholle trifft, wenn nicht fällt er hinunter
+		//penguin wird auf exakte(kann je nah frame auch etwas darunter sein) Schollenhöhe, wenn er die Scholle trifft, wenn nicht fällt er hinunter
 		if (penguin.y + penguin.height >= iceFloe1.y && penguin.x < iceFloe1.x + iceFloe1.width && penguin.x > iceFloe1.x) {
-			penguin.vy = -150.0
 			penguin.y = iceFloe1.y - penguin.height
+		}
+
+		//penguin springt mit vy 150 ab, wenn man die Pfeiltaste nach oben drückt
+		if (input.keys[Key.UP]) {
+			penguin.vy = -150.0
 		}
 
 	}
